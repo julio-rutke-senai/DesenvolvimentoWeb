416ec9ed54c63d1b2e7bca836c8002832b107fb7
professor <professor@SN-371156.edu.fiesc.com.br>
2024-04-18 20:42:34 -0300

    Manipulação DOM

---
+++ Exemplos/EstruturaBasica/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var links = document.getElementsByClassName("a");
+
+    for(var i = 0; i < links.length; i++){
+        links[i].setAttribute("class", "link");
+    }
+
+}
